1a40392ad6e4f8dccd2bd83732babd790a5c5f5c
SAFAA1981 <safaasaadoon@gmail.com>
2025-07-06 21:08:39 +0300

    test3

--- main.py
+++ main.py
@@ -7,9 +7,3 @@ app = create_app()
 
 if __name__ == "__main__":
     app.run(debug=True)
-=======
-create_app()
->>>>>>> f88e617cec8518451e328326facaf907971b4757
-=======
-create_app()
->>>>>>> f88e617cec8518451e328326facaf907971b4757
