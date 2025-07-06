ee8aef888cbbc0ea2947a7882a985ce7fa1b6677
ArduinoBoy1111 <safaa5safaa12345@gmail.com>
2025-07-06 23:46:43 +0300

    user and admin login system

--- website/views.py
+++ website/views.py
@@ -8,14 +8,9 @@ views = Blueprint("views", __name__)
 depts = 3000000
 paid = 2500000
 
-prev_items = [
-    {"name": "سمنت", "price": 150000},
-    {"name": "لبخ", "price": 125000},
-]
-
 
 # Home page
-@views.route("/", methods=["GET"])
+@views.route("/")
 def home():
     return render_template("index.html")
 
@@ -23,48 +18,63 @@ def home():
 # Debt tracker view
 @views.route("/debt-tracker")
 def debt_tracker():
-    return render_template(
-        "debt tracker.html", depts=depts, paid=paid, prev_items=prev_items
-    )
+    return render_template("debt tracker.html", depts=depts, paid=paid)
 
 
 # Financial information page showing transactions
 @views.route("/financial-info")
 def financial_info():
-    debts = Transaction.query.filter_by(debt="1").all()
+    debts = Transaction.query.filter_by(debt="True").all()
     payments = Transaction.query.filter_by(debt="False").all()
     return render_template("financial info.html", debts=debts, payment=payments)
 
 
 # Admin login route
-@views.route("/admin-page", methods=["GET", "POST"])
-def admin_page():
+@views.route("/admin-login", methods=["GET", "POST"])
+def admin_login():
     if request.method == "POST":
         name = request.form.get("name")
         code = request.form.get("code")
 
         user = User.query.filter_by(name=name, code=code).first()
 
-        if user and user.admin == "1":
+        if user and user.admin == True:
             session["admin"] = True
             session["username"] = user.name
+            session["code"] = user.code
+
             return redirect("/admin-dashboard")
         else:
             return render_template("admin login.html", error="Access denied.")
+    else:
+        return render_template("admin login.html")
 
-    return render_template("admin login.html")
+
+@views.route("/user-login", methods=["GET", "POST"])
+def user_login():
+    if request.method == "POST":
+        name = request.form.get("name")
+        code = request.form.get("code")
+
+        user = User.query.filter_by(name=name, code=code).first()
+
+        if user:
+            session["admin"] = False
+            session["username"] = user.name
+            session["code"] = user.code
+            session["user_id"] = user.id
+
+            return redirect("/debt-tracker")
+        else:
+            return render_template("user login.html", error="Access denied.")
+    else:
+        return render_template("user login.html")
 
 
-# Admin dashboard - protected
 @views.route("/admin-dashboard")
 def admin_dashboard():
-    if not session.get("admin"):
-        return redirect("/admin-page")
-    return render_template("admin_dashboard.html")
+    if session["admin"]:
+        return render_template("admin dashboard.html")
 
-
-# Logout route
-@views.route("/logout")
-def logout():
-    session.clear()
-    return redirect("/")
+    else:
+        return redirect("/admin-login")
